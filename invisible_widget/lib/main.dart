a9aab759be7eb259fb9b56993815f2877871098e
DRACKWARS <dionpratama153@gmail.com>
2023-10-19 01:03:56 +0700

    Summary

---
+++ invisible_widget/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("Column")),
+          body: Stack(
+            children: [
+              Container(
+                height: 400,
+                width: 400,
+                color: Colors.green,
+              ),
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.purple,
+              ),
+            ],
+          )),
+    );
+  }
+}
